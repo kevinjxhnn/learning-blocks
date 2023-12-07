7d314e0cd12eeccdacb4693490ca78f2a7fb276f
Kevin <kevinjxhn@Kevins-MacBook-Air.local>
2023-12-07 22:22:59 +0530

    working template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+import React from "react";
 
 function App() {
+  const editor = useBlockNote({});
+  const [inputObject, setInputObject] = React.useState();
+  const [intialBlock, setInitialBlock] = React.useState();
+
+  editor.onEditorContentChange(() => {
+    const blocks = editor.topLevelBlocks;
+    setInputObject(blocks);
+    setInitialBlock(blocks[0].id);
+  });
+
+  editor.on
+
+  const object = [
+    {
+      id: "bee938a8-18c1-4bc4-8240-ed7ba440a65c",
+      type: "paragraph",
+      props: {
+        textColor: "default",
+        backgroundColor: "default",
+        textAlignment: "left",
+      },
+      content: [
+        {
+          type: "text",
+          text: "Hello",
+          styles: {},
+        },
+      ],
+      children: [],
+    },
+    {
+      id: "afb7b3a1-923d-420b-a7dc-bce8c023dc76",
+      type: "paragraph",
+      props: {
+        textColor: "default",
+        backgroundColor: "default",
+        textAlignment: "left",
+      },
+      content: [],
+      children: [],
+    },
+  ];
+
+  const blocksToInsert = object.map((block) => ({
+    id: block.id,
+    type: block.type,
+    props: block.props,
+    content: block.content,
+    children: block.children,
+  }));
+
+  // if (intialBlock) {
+  //   console.log("id", intialBlock);
+  //   editor.insertBlocks(blocksToInsert, intialBlock);
+  // }
+
+  React.useEffect(() => {
+    if (intialBlock) {
+      console.log("id", intialBlock);
+      console.log(intialBlock)
+      editor.insertBlocks(blocksToInsert, intialBlock, "before");
+    }
+  }, [intialBlock]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BlockNoteView editor={editor} theme={"dark"} />
+      <button onClick={() => console.log(inputObject)}>on submit</button>
+    </>
   );
 }
 
